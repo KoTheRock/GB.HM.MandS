5052fb91a5d5628d835113a72f4cb6b10d1b09e7
Ilya Katyshov <creepertp@gmail.com>
2023-12-11 14:34:48 +0300

    first commit

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+public class Program
+{
+    static void Main()
+    {
+        char[,] charArray = {
+        
+        {'3', 'h', 'm', '2', ')'},
+        {'=', 'g', '2', '/', '4',}
+        };
+
+        string result = string.Empty;
+
+        int rows = charArray.GetLength(0);
+        int cols = charArray.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                result += charArray[i, j];
+            }
+        }
+        Console.WriteLine("Строка из массива символов: " + result);
+    }
+}
---
+++ Zadanie2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+
+public class Program
+{
+    static void Main()
+    {
+        string symbols = new string(new char[] { 'a', 'b', 'C', 'd', 'F', 'G', 'h', 'i', 'j', 'K', 'l', 'M', 'n', 'O', 'p', 'Q', 'R', 's', 'T' });
+
+        string lowerCase = symbols.ToLower();
+  
+        Console.WriteLine("Строка из массива символов: " + symbols);
+        Console.WriteLine("Строка из массива строчных символов : " + lowerCase);
+        
+    }
+}
