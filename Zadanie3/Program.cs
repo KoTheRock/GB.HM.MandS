b44b14cd62ce3d0fc304423873d33e11d9dfd313
KoTheRock <149530900+KoTheRock@users.noreply.github.com>
2023-12-11 14:42:59 +0300

    Add files via upload

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте произвольную строку. Выясните, является ли она палиндромом
+
+public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите строку :");
+        string input = Console.ReadLine();
+
+        bool Palindrome = IsPalindrome(input);
+        Console.WriteLine($"Строка \"{input}\" является палиндромом");
+    }
+    static bool IsPalindrome(string input)
+    {
+        int left = 0;
+        int right = input.Length - 1;
+
+        while (left < right)
+        {
+            if (input[left] != input[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
